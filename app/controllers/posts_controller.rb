9ba3f3321fdbe70655f360d3561e45b64b934a19
Matt Davis <matt.davis129@gmail.com>
2014-11-21 16:38:55 -0800

    adds delete function for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,25 @@
 class PostsController < ApplicationController
   before_action :require_login
+ 
 
 	def create
     @post = User.find(params[:id]).posts.build(post_params)
     if @post.save
     	flash[:success] = "Posted!"
-    	redirect_to current_user
+    	redirect_to @post.user
     else
     	render 'static_pages/home'
     end
 	end
 
 	def destroy
-
+    @post = current_user.posts.find_by(id: params[:id])
+    if @post.destroy
+      flash[:success] = "Post deleted."
+      redirect_to @post.user
+    else
+    	redirect_to @post.user
+    end
 	end
 
 	private
@@ -20,5 +27,6 @@ class PostsController < ApplicationController
 	def post_params
 		params.require(:post).permit(:content)
 	end
+
 
 end
