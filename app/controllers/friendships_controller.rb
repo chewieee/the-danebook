83841e7ad892cbbac7fdea6ebc6d7868a9dd4184
Matt Davis <matt.davis129@gmail.com>
2014-11-23 12:21:53 -0800

    adds unfriend me button to friends index

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,14 +1,16 @@
 class FriendshipsController < ApplicationController
 	
+	before_action :set_return_path, only: [:create, :destroy]
+
   def create
     @user = User.find(params[:friendship][:friended_id])
 	  current_user.friend!(@user)
-	  redirect_to @user
+	  redirect_to session.delete(:return_to)
 	end
 
 	def destroy
 		@user = Friendship.find(params[:id]).friended
 		current_user.unfriend!(@user)
-		redirect_to @user
+		redirect_to session.delete(:return_to)
 	end
 end
